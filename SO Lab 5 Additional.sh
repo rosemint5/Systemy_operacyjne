a40d3e6515472d47b29cb28bdd23a849a805a7db
M4G1-K4RP <jak.karpowicz@gmail.com>
2025-01-19 15:19:59 +0100

    Add files via upload

---
+++ SO Lab 5 Additional.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#sprawdzanie dysków
+available_disks=$(lsblk -o NAME,SIZE,TYPE | grep disk)
+
+#sprawdzanie partycji utworzonej podczas labolatorium
+part="/dev/vdb1"
+size=$(df -h | grep "$part")
+
+#Nazwa pliku i ściezka końcowa
+date_and_time=$(date +"%Y%m%d-%H%M%S")
+final_name="Report_${date_and_time}.txt"
+final_file_path="./${final_name}"
+
+#Zapisanie do pliku
+{
+  echo "Dostępne dyski: "
+  echo "$available_disks"
+  echo
+  echo "Dostępne miejsce na partycji $part: "
+  echo "$size"
+  echo
+} > "$final_file_path"
+
+#Komunikat końcowy
+echo "Plik o nazwie $final_name został zapisany w $(pwd)."
